668c8754e83d11b504105fc420c9ae54cb901785
lexathar <93197848+lexathar@users.noreply.github.com>
2021-12-22 16:07:42 +0000

    second push

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-/* 
-👉 Write your kata here!
-*/
+// EEExcEEEllEEEnt strings
+// you've been looking at some memes and you came across the famous E meme. As a result, you would like to automate your comments on YouTube, giving them extra E's. However, your custom mechanical keyboard only has a limited amount of keypresses before it breaks the switch. Your function should replace every e with 3 capital E's.
+// example - Jasien's lesson was interesting
+// JasiEEEns lEEEsson was intEEErEEEsting
+
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+// module.exports = eeeNator();
+
+module.exports = function eeeNator() {
+// your function goes here!
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- main.test.js
+++ main.test.js
@@ -1,5 +1,4 @@
-import { TestWatcher } from "jest"
-import { eeeNator } from "./main.js"
+const eeeNator = require('./main.js')
 
 //👉 Write your tests below here:
 
@@ -11,5 +10,31 @@ import { eeeNator } from "./main.js"
 //compare string input after being passed through the function, it should be strictly equal to the desired outcome
 
 test(`Should replace every instance of "e" with 3 "E"s of any given string`, () => {
-    const actual
-}
+    const actual = eeeNator("Hello there, sir");
+    const expected = "HEEEllo thEEErEEE, sir";
+
+    expect(actual).toBe(expected);
+});
+
+test(`Should replace every instance of "e" with 3 "E"s of any given string`, () => {
+    const actual = eeeNator("Why is the sun so bright?");
+    const expected = "Why is the sun so bright?";
+
+    expect(actual).toBe(expected);
+});
+
+test(`Should replace every instance of "e" with 3 "E"s of any given string`, () => {
+    const actual = eeeNator("Why do my feet smell if they don't have a nose?");
+    const expected = "Why do my fEEEEEEt smEEEll if thEEEy don't havEEE a nosEEE?";
+
+    expect(actual).toBe(expected);
+});
+
+test(`Should replace every instance of "e" with 3 "E"s of any given string`, () => {
+    const actual = eeeNator("Testing is terrifying!");
+    const expected = "TEEEsting is tEEErrifying!";
+
+    expect(actual).toBe(expected);
+});
+
+
