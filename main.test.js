626aa26098814274a64703518d4d0c67b08b04c2
lexathar <93197848+lexathar@users.noreply.github.com>
2021-12-22 15:36:17 +0000

    1st push
    1st push

--- main.test.js
+++ main.test.js
@@ -1,1 +1,15 @@
+import { TestWatcher } from "jest"
+import { eeeNator } from "./main.js"
+
 //👉 Write your tests below here:
+
+
+
+
+
+//what should the test do?
+//compare string input after being passed through the function, it should be strictly equal to the desired outcome
+
+test(`Should replace every instance of "e" with 3 "E"s of any given string`, () => {
+    const actual
+}
