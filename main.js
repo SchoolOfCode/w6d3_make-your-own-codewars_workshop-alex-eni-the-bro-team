7c51c7d567ac4f1f573efd0bde1ac027c566f0e4
lexathar <93197848+lexathar@users.noreply.github.com>
2021-12-23 14:21:58 +0000

    final final

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
 // EEExcEEEllEEEnt strings
-// you've been looking at some memes and you came across the famous E meme. As a result, you would like to automate your comments on YouTube, giving them extra E's. However, your custom mechanical keyboard only has a limited amount of keypresses before it breaks the switch. Your function should replace every e with 3 capital E's.
-// example - Jasien's lesson was interesting
-// JasiEEEns lEEEsson was intEEErEEEsting
+// You've been looking at some memes and you came across the famous E meme. You want to post some epic E related comments, but your mechanical keyboard can't handle that many E presses! As a result, you would like to increase your E count without it wearing down your keyboard. Your function should replace every e/E with 3 capital E's.
+// You will always be given a string which contains only the English alphabet, with both upper case and lower case characters. 
+// Example: "Jasien's lesson was interesting"
+// Should return: "JasiEEEn's lEEEsson was intEEErEEEsting"
 
-
-
-//👉 Write the function your CodeWarriors will start with below here:
-
-// module.exports = eeeNator();
 
 module.exports = function eeeNator(string) {
 // your function goes here!
-
-    return (string.replace(/[eE]/g,  "EEE"));
 }
 
 
